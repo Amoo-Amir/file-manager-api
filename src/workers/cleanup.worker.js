134197f22c22c8bd26204a4b050e835c0eef6595
AmirmahdiRahmati <amirmahdiamirmahdi774@gmail.com>
2026-06-07 21:21:13 +0330

    Update cleanup.worker.js
    no change.

--- src/workers/cleanup.worker.js
+++ src/workers/cleanup.worker.js
@@ -3,16 +3,13 @@ const path = require("path");
 const File = require("../models/file.model");
 const logger = require("../utils/logger");
 
-// فایل‌هایی که بیشتر از RETENTION_DAYS روز پیش حذف شدن رو از disk پاک کن
 const RETENTION_DAYS = 7;
 
 const cleanupDeletedFiles = async () => {
   logger.info("Cleanup worker started");
 
   const cutoff = new Date(Date.now() - RETENTION_DAYS * 24 * 60 * 60 * 1000);
-
-  // فایل‌هایی که isDeleted: true و deletedAt قدیمی‌تر از cutoff هستن
-  // چون pre(/^find/) رو حذف کردیم، باید مستقیم query بزنیم
+  
   const expiredFiles = await File.find({
     isDeleted: true,
     deletedAt: { $lt: cutoff },
@@ -30,13 +27,11 @@ const cleanupDeletedFiles = async () => {
 
   for (const file of expiredFiles) {
     try {
-      // حذف از disk
       const absolutePath = path.resolve(file.path);
       if (fs.existsSync(absolutePath)) {
         fs.unlinkSync(absolutePath);
       }
 
-      // حذف از DB
       await File.deleteOne({ _id: file._id });
 
       deleted++;
